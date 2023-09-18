2cf28fe83b4c0fc39ff583af6ddbf1ae7851ea01
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-09-18 12:21:11 -0400

    try

--- build.sh
+++ build.sh
@@ -1,3 +1,31 @@
+# set -e
+
+# COVERAGE_FLAGS="-g --coverage -fprofile-arcs -ftest-coverage"
+
+# # make install for these subdirectories
+# MAKE_INSTALL_DIRS=(
+#     "src"
+#     "utils"
+
+# )
+
+# # only make for these subdirectories
+# MAKE_ONLY_DIRS=(
+#     "src"
+# )
+
+# for DIR in "${MAKE_INSTALL_DIRS[@]}" "${MAKE_ONLY_DIRS[@]}"; do
+#     mkdir /home/ns3_c-v2x/"$DIR"/build
+#     cd /home/ns3_c-v2x/"$DIR"/build
+#     cmake -DCMAKE_CXX_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_C_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_BUILD_TYPE="Debug" ..
+#     make -j
+#     for MAKE_INSTALL_DIR in "${MAKE_INSTALL_DIRS[@]}"; do
+#         if [ "$DIR" == "$MAKE_INSTALL_DIR" ]; then
+#             make -j install
+#         fi
+#     done
+# done
+
 #!/bin/bash
 #  Copyright (C) 2018-2020 LEIDOS.
 # 
@@ -13,30 +41,13 @@
 #  License for the specific language governing permissions and limitations under
 #  the License.
 
+# script executes all kafka_clients and scheduling service build and coverage steps so that they can be singularly
+# wrapped by the sonarcloud build-wrapper
 set -e
 
-COVERAGE_FLAGS="-g --coverage -fprofile-arcs -ftest-coverage"
+mkdir build
+cd build
+cmake ..
+cmake --build .
 
-# make install for these subdirectories
-MAKE_INSTALL_DIRS=(
-    "src"
-    "utils"
-
-)
-
-# only make for these subdirectories
-MAKE_ONLY_DIRS=(
-    "src"
-)
-
-for DIR in "${MAKE_INSTALL_DIRS[@]}" "${MAKE_ONLY_DIRS[@]}"; do
-    mkdir /home/ns3_c-v2x/"$DIR"/build
-    cd /home/ns3_c-v2x/"$DIR"/build
-    cmake -DCMAKE_CXX_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_C_FLAGS="${COVERAGE_FLAGS}" -DCMAKE_BUILD_TYPE="Debug" ..
-    make -j
-    for MAKE_INSTALL_DIR in "${MAKE_INSTALL_DIRS[@]}"; do
-        if [ "$DIR" == "$MAKE_INSTALL_DIR" ]; then
-            make -j install
-        fi
-    done
-done
+ctest --output-on-failure
